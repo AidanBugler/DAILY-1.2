f2073414ac95573ce45e43bfebce67cf5b5a9a04
AidanBugler <buglaida982@gotvdsb.ca>
2020-11-23 15:10:52 +0000

    ADDED VAR

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 class Main {
   public static void main(String[] args) {
   //VAR
+  Scanner input = new Scanner(System.in);
+    String animalType;
+    int intalAnimal; //The Intal Amount of Aminals
+    int intalYear = 2020;
+    int finalYear; //The Projected Year
+    int finalAnimal; //The Projected Amount of Animals 
   
   
   
@@ -11,9 +17,7 @@ class Main {
   
   
   
-  
-  
-  System.out.println("");
+    System.out.println("");
   
   
   }
